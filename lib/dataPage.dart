dad1a8af2435763ccc73ce494fa48a2805bec07b
MIA <75183183+Ereshkigallll@users.noreply.github.com>
2024-04-16 02:37:24 +0100

    data average function complete

--- lib/dataPage.dart
+++ lib/dataPage.dart
@@ -33,8 +33,7 @@ class TitleSection extends StatelessWidget {
     final double horizontalPadding = screenWidth * 0.01;
     final double verticalPadding = screenHeight * 0.01;
     return Padding(
-      padding: EdgeInsets.only(
-          top: 8 * verticalPadding),
+      padding: EdgeInsets.only(top: 8 * verticalPadding),
       child: const Text(
         'Tomato Analysis',
         style: TextStyle(
@@ -170,7 +169,10 @@ class _FocusTimeCardState extends State<FocusTimeCard> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(isSwitched ? 'Total Tomatoes' : 'Total Focus Time',  // 根据 isSwitched 显示不同的文本
+              Text(
+                  isSwitched
+                      ? 'Total Tomatoes'
+                      : 'Total Focus Time', // 根据 isSwitched 显示不同的文本
                   style: TextStyle(
                     color: Color(0xffEF7453),
                     fontSize: 22,
@@ -299,13 +301,15 @@ class _FocusTimeCardState extends State<FocusTimeCard> {
                       fontWeight: FontWeight.w800,
                       color: Color(0xffFFF5F1),
                     )),
-                !isSwitched ? Text(" Mins",
-          style: TextStyle(
-            fontSize: 14,
-            fontFamily: 'Inter-Display',
-            fontWeight: FontWeight.w800,
-            color: Color(0xffFFF5F1),
-          )) : Container(),
+                !isSwitched
+                    ? Text(" Mins",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Inter-Display',
+                          fontWeight: FontWeight.w800,
+                          color: Color(0xffFFF5F1),
+                        ))
+                    : Container(),
               ],
             ),
           ],
@@ -323,18 +327,36 @@ class AverageTimeCard extends StatefulWidget {
 class _AverageTimeCardState extends State<AverageTimeCard> {
   bool isSwitched = false;
   int totalTimeMinutes = 0;
-  int totalCountNum = 0; // 总记录条数
-  int thisMonthTotalMinutes = 0;
-  int thisMonthCount = 0; // 本月记录条数
-  int thisWeekTotalMinutes = 0;
-  int thisWeekCount = 0; // 本周记录条数
-  int todayTotalMinutes = 0;
-  int todayCount = 0; // 今日记录条数
+  Set<String> uniqueYears = Set();
+  Set<String> uniqueMonths = Set();
+  Set<String> uniqueWeeks = Set();
+  Set<String> uniqueDays = Set();
+
+  double averageYearlyMinutes = 0;
+  double averageMonthlyMinutes = 0;
+  double averageWeeklyMinutes = 0;
+  double averageDailyMinutes = 0;
+
+  int yearlyRecordCount = 0;
+  int monthlyRecordCount = 0;
+  int weeklyRecordCount = 0;
+  int dailyRecordCount = 0;
+
+  double averageYearlyCount = 0;
+  double averageMonthlyCount = 0;
+  double averageWeeklyCount = 0;
+  double averageDailyCount = 0;
 
   @override
   void initState() {
     super.initState();
     fetchTotalTime();
+  }
+
+  int getWeekOfYear(DateTime date) {
+    int dayOfYear = int.parse(DateFormat("D").format(date));
+    int woy = ((dayOfYear - date.weekday + 10) / 7).floor();
+    return woy;
   }
 
   void fetchTotalTime() {
@@ -353,13 +375,15 @@ class _AverageTimeCardState extends State<AverageTimeCard> {
     ref.onValue.listen((DatabaseEvent event) {
       final data = event.snapshot.value as Map<dynamic, dynamic>?;
       int totalMinutes = 0;
-      int totalCount = 0;
-      int monthMinutes = 0;
-      int monthCount = 0;
-      int weekMinutes = 0;
-      int weekCount = 0;
-      int dayMinutes = 0;
-      int dayCount = 0;
+      Map<String, int> yearlyRecords = {};
+      Map<String, int> monthlyRecords = {};
+      Map<String, int> weeklyRecords = {};
+      Map<String, int> dailyRecords = {};
+
+      uniqueYears.clear();
+      uniqueMonths.clear();
+      uniqueWeeks.clear();
+      uniqueDays.clear();
 
       if (data != null) {
         data.forEach((key, value) {
@@ -372,46 +396,46 @@ class _AverageTimeCardState extends State<AverageTimeCard> {
             List<String> parts = time.split(':');
             if (parts.length == 2) {
               int minutes = int.tryParse(parts[0]) ?? 0;
-
-              // Accumulate total minutes
               totalMinutes += minutes;
-              totalCount++;
 
-              // Check and accumulate for current month
-              if (timestamp.isAfter(startOfCurrentMonth) &&
-                  timestamp.isBefore(DateTime(now.year, now.month + 1))) {
-                monthMinutes += minutes;
-                monthCount++;
-              }
+              String year = "${timestamp.year}";
+              String month = "${timestamp.year}-${timestamp.month}";
+              String week = "${timestamp.year}-W${getWeekOfYear(timestamp)}";
+              String day =
+                  "${timestamp.year}-${timestamp.month}-${timestamp.day}";
 
-              // Check and accumulate for current week
-              if (timestamp.isAfter(startOfCurrentWeek) &&
-                  timestamp
-                      .isBefore(startOfCurrentWeek.add(Duration(days: 7)))) {
-                weekMinutes += minutes;
-                weekCount++;
-              }
+              uniqueYears.add(year);
+              uniqueMonths.add(month);
+              uniqueWeeks.add(week);
+              uniqueDays.add(day);
 
-              // Check and accumulate for today
-              if (timestamp.year == now.year &&
-                  timestamp.month == now.month &&
-                  timestamp.day == now.day) {
-                dayMinutes += minutes;
-                dayCount++;
-              }
+              yearlyRecords.update(year, (value) => value + 1,
+                  ifAbsent: () => 1);
+              monthlyRecords.update(month, (value) => value + 1,
+                  ifAbsent: () => 1);
+              weeklyRecords.update(week, (value) => value + 1,
+                  ifAbsent: () => 1);
+              dailyRecords.update(day, (value) => value + 1, ifAbsent: () => 1);
             }
           }
         });
       }
+
       setState(() {
-        totalTimeMinutes = totalMinutes;
-        totalCountNum = totalCount;
-        thisMonthTotalMinutes = monthMinutes;
-        thisMonthCount = monthCount;
-        thisWeekTotalMinutes = weekMinutes;
-        thisWeekCount = weekCount;
-        todayTotalMinutes = dayMinutes;
-        todayCount = dayCount;
+        averageYearlyMinutes = (totalMinutes / uniqueYears.length);
+        averageMonthlyMinutes = totalMinutes / uniqueMonths.length;
+        averageWeeklyMinutes = totalMinutes / uniqueWeeks.length;
+        averageDailyMinutes = totalMinutes / uniqueDays.length;
+
+        yearlyRecordCount = yearlyRecords.values.fold(0, (a, b) => a + b);
+        monthlyRecordCount = monthlyRecords.values.fold(0, (a, b) => a + b);
+        weeklyRecordCount = weeklyRecords.values.fold(0, (a, b) => a + b);
+        dailyRecordCount = dailyRecords.values.fold(0, (a, b) => a + b);
+
+        averageYearlyCount = yearlyRecordCount / uniqueYears.length;
+        averageMonthlyCount = monthlyRecordCount / uniqueMonths.length;
+        averageWeeklyCount = weeklyRecordCount / uniqueWeeks.length;
+        averageDailyCount = dailyRecordCount / uniqueDays.length;
       });
     });
   }
@@ -436,7 +460,10 @@ class _AverageTimeCardState extends State<AverageTimeCard> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(isSwitched ? 'Average Tomatoes' : 'Average Focus Time',  // 根据 isSwitched 显示不同的文本
+              Text(
+                  isSwitched
+                      ? 'Average Tomatoes'
+                      : 'Average Focus Time', // 根据 isSwitched 显示不同的文本
                   style: TextStyle(
                     color: Color(0xffEF7453),
                     fontSize: 22,
@@ -490,16 +517,18 @@ class _AverageTimeCardState extends State<AverageTimeCard> {
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     _buildTimeBox(
-                        'Until Now',
-                        isSwitched ? '$totalCountNum' : '$totalTimeMinutes',
+                        'Yearly',
+                        isSwitched
+                            ? '${averageYearlyCount.toStringAsFixed(1)}'
+                            : '${averageYearlyMinutes.toStringAsFixed(1)}',
                         screenWidth * 0.4,
                         screenHeight * 0.1),
                     SizedBox(width: horizontalPadding * 3),
                     _buildTimeBox(
-                        'This Month',
+                        'Monthly',
                         isSwitched
-                            ? '$thisMonthCount'
-                            : '$thisMonthTotalMinutes',
+                            ? '${averageMonthlyCount.toStringAsFixed(1)}'
+                            : '${averageMonthlyMinutes.toStringAsFixed(1)}',
                         screenWidth * 0.4,
                         screenHeight * 0.1),
                   ],
@@ -509,14 +538,18 @@ class _AverageTimeCardState extends State<AverageTimeCard> {
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     _buildTimeBox(
-                        'This Week',
-                        isSwitched ? '$thisWeekCount' : '$thisWeekTotalMinutes',
+                        'Weekly',
+                        isSwitched
+                            ? '${averageWeeklyCount.toStringAsFixed(1)}'
+                            : '${averageWeeklyMinutes.toStringAsFixed(1)}',
                         screenWidth * 0.4,
                         screenHeight * 0.1),
                     SizedBox(width: horizontalPadding * 3),
                     _buildTimeBox(
-                        'Today',
-                        isSwitched ? '$todayCount' : '$todayTotalMinutes',
+                        'Daily',
+                        isSwitched
+                            ? '${averageDailyCount.toStringAsFixed(1)}'
+                            : '${averageDailyMinutes.toStringAsFixed(1)}',
                         screenWidth * 0.4,
                         screenHeight * 0.1),
                   ],
@@ -565,13 +598,15 @@ class _AverageTimeCardState extends State<AverageTimeCard> {
                       fontWeight: FontWeight.w800,
                       color: Color(0xffFFF5F1),
                     )),
-                !isSwitched ? Text(" Mins",
-          style: TextStyle(
-            fontSize: 14,
-            fontFamily: 'Inter-Display',
-            fontWeight: FontWeight.w800,
-            color: Color(0xffFFF5F1),
-          )) : Container(),
+                !isSwitched
+                    ? Text(" Mins",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Inter-Display',
+                          fontWeight: FontWeight.w800,
+                          color: Color(0xffFFF5F1),
+                        ))
+                    : Container(),
               ],
             ),
           ],
